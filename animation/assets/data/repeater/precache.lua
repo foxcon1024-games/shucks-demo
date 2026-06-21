f37cb2917f873d9b47e4676a5ce5a09adb882256
foxcon1024-games <foxcon1024@gmail.com>
2026-06-21 02:34:44 -0500

    Delete animation directory

--- animation/assets/data/repeater/precache.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-    --precache
-    --dad
-    addCharacterToList('yellow-block', 'dad');
-    addCharacterToList('YellowAndBlock', 'dad');
-end
--- animation/assets/data/stickin-to-it/precache.lua
+++
@@ -1,5 +0,0 @@
-function onCreate()
-    --precache
-    --dad
-    addCharacterToList('green-guitar', 'dad');
-end
